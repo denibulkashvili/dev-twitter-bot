7412a5b1ade99277b5b2fac752d93fc1b2300f17
denibulkashvili <denibulkashvili@gmail.com>
2018-12-18 04:19:44 +0700

    Put bot logic in while loop and update interval

--- bot.py
+++ bot.py
@@ -15,25 +15,23 @@ access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
 
-# INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 15  # every 15 seconds, for testing
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-api_quote = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
-api_quote_id = api_quote["id"]
-api_quote_author = api_quote["author"]
-api_quote_text = api_quote["quote"]
-
-
-post = f'{api_quote_text} - {api_quote_author}'
-
-
-api.update_status(post)
-print(f'Post tweeted:\n{post}')
-time.sleep(INTERVAL) 
+while True:
+    api_quote = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
+    api_quote_id = api_quote["id"]
+    api_quote_author = api_quote["author"]
+    api_quote_text = api_quote["quote"]
+    post = f'{api_quote_text} - {api_quote_author}'
+    
+    api.update_status(post)
+    print(f'Post tweeted:\n{post}')
+    time.sleep(INTERVAL) 
 
 
