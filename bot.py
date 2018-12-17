92193df560958b54a7193782043487840736446a
denibulkashvili <denibulkashvili@gmail.com>
2018-12-18 04:25:55 +0700

    Refactor while loop

--- bot.py
+++ bot.py
@@ -8,12 +8,12 @@ load_dotenv()
 import os
 
 
+# Bot Settings
 consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
 
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
-
 
 INTERVAL = 60 * 60 * 6  # tweet every 6 hours
 # INTERVAL = 15  # every 15 seconds, for testing
@@ -23,15 +23,22 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-while True:
+def getQuote():
     api_quote = requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
     api_quote_id = api_quote["id"]
     api_quote_author = api_quote["author"]
     api_quote_text = api_quote["quote"]
     post = f'{api_quote_text} - {api_quote_author}'
-    
+    return post
+
+def tweet(post):
     api.update_status(post)
     print(f'Post tweeted:\n{post}')
     time.sleep(INTERVAL) 
+
+while True:
+    post = getQuote()
+    tweet(post)
+    
 
 
